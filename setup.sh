346e2ec02dd3c25fa92f0f5aca3f28775567b558
AmberLJC <amberljc@umich.edu>
2020-04-15 17:41:13 -0400

    chmod

--- setup.sh
+++ setup.sh
@@ -2,4 +2,11 @@ pip install --upgrade pip
 pip install torch torchvision  numpy
 
 
+apt install nvidia-cuda-toolkit -y
+sudo apt upgrade -y
+
+sudo rmmod nvidia_uvm
+sudo rmmod nvidia
+sudo modprobe nvidia
+sudo modprobe nvidia_uvm
 
